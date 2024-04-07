0a9daab98d761253d3247231010f72a5f19ef686
Tobias Schncak <tobias.schnack@gmail.com>
2024-04-07 15:35:37 +0200

    add momentum and xG time series

--- src/slpredictions/tools/__init__.py
+++ src/slpredictions/tools/__init__.py
@@ -1,20 +1,82 @@
 import logging
 import pandas as pd
 import duckdb as db
-from .data import get_seasons, get_matches, get_match_data
+from .data import get_seasons, get_matches, get_match_stats, get_momentum, get_xg_time
 
-l = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def main() -> int:
-    l.info("Starting database update.")
-    with db.connect("sl.db") as con:
-        # setup tables
-        con.sql(
-            "CREATE TABLE IF NOT EXISTS SEASONS(id int64 PRIMARY KEY, year varchar);"
-        )
-        con.sql(
-            """CREATE TABLE IF NOT EXISTS Matches(
+    logger.info("Starting database update.")
+    with db.connect("src/sl.db") as con:
+        setup_tables(con)
+
+        logger.info("Getting seasons...")
+        seasons_df = get_seasons()
+        no_of_seasons = seasons_df["id"].count()
+        con.sql("INSERT OR IGNORE INTO Seasons BY NAME SELECT * FROM seasons_df")
+        logger.info("Done getting and saving seasons.")
+
+        # iterate over all seasons:
+        for s_idx, season_row in seasons_df.iterrows():
+            season_id = season_row["id"]
+            logger.info(
+                "Getting matches for season %s out of %s seasons.",
+                season_row["year"],
+                no_of_seasons,
+            )
+
+            # Check if matches already exists if not download matches.
+            logger.info("Getting matches for season id %s...", season_id)
+            #if not con.sql(
+            #    f"SELECT * FROM Matches WHERE tournamentId = {season_id} LIMIT 1;"
+            #).fetchone():
+            #    logger.info("season %s already in database. Skipping.", season_id)
+            #    continue
+
+            matches_df = get_matches(season_id)
+
+            if matches_df is None:
+                logger.warning("Failed to get matches for %s!", season_id)
+                continue
+            no_of_matches = matches_df["eventId"].count()
+            con.sql("INSERT OR IGNORE INTO Matches BY NAME SELECT * FROM matches_df")
+
+            logger.info("Gettingg match stats for %s matches...", no_of_matches)
+            for idx, match_row in matches_df.iterrows():
+                if idx % 10 == 0:
+                    logger.info("Fetting stats for match %s of %s.", idx, no_of_matches)
+                if match_row["statusType"] == "finished":
+                    if match_row["hasOpta"] is True:  # hasOpta can be nan
+                        match_stats_df = get_match_stats(match_row["eventId"])
+                        if match_stats_df is not None and not match_stats_df.empty:
+                            con.sql(
+                                "INSERT INTO MatchStats BY NAME SELECT * FROM match_stats_df"
+                            )
+
+                        match_xg_df = get_xg_time(match_row["eventId"])
+                        if match_xg_df is not None and not match_xg_df.empty:
+                            con.sql(
+                                "INSERT INTO MatchXG BY NAME SELECT * FROM match_xg_df"
+                            )
+
+                    if match_row["hasOptaMomentum"] is True:  # hasMomentum can be nan
+                        match_momentum_df = get_momentum(match_row["eventId"])
+                        if match_momentum_df is not None and not match_momentum_df.empty:
+                            con.sql(
+                                "INSERT INTO MatchMomentum BY NAME SELECT * FROM match_momentum_df"
+                            )
+
+            logger.info("Done getting data for all matches for %s.", season_id)
+
+    logger.info("Done")
+    return 0
+
+
+def setup_tables(con):
+    con.sql("CREATE TABLE IF NOT EXISTS SEASONS(id int64 PRIMARY KEY, year varchar);")
+    con.sql(
+        """CREATE TABLE IF NOT EXISTS Matches(
                 tournamentId int64 REFERENCES Seasons(id), 
                 eventId int64 PRIMARY KEY,
                 roundNr int8, 
@@ -28,63 +90,37 @@ def main() -> int:
                 hasOpta boolean,
                 hasOptaMomentum boolean,
                 statusType varchar
-        );""".replace("\n", "")
-        )
-        con.sql(
-            "CREATE TABLE IF NOT EXISTS MatchData(teamId int16, eventId int64 REFERENCES Matches(eventId), variable varchar, value double);"
-        )
-
-        seasons_df = get_seasons()
-        # seasons_df = seasons_df.tail(2)        # take last n seasons
-        # seasons_df = seasons_df.iloc[::-1]     # reverse order, so latest season is first
-        no_of_seasons = seasons_df["id"].count()
-
-        con.sql("INSERT OR IGNORE INTO Seasons BY NAME SELECT * FROM seasons_df")
-
-        # iterate over all seasons:
-        for s_idx, season_row in seasons_df.iterrows():
-            l.info("Getting season %s out of %s.", season_row["year"], no_of_seasons)
-            season_id = season_row["id"]
-            l.info("Getting matches for season id %s...", season_id)
-
-            if not con.sql(
-                f"SELECT * FROM Matches WHERE tournamentId = {season_id} LIMIT 1;"
-            ).fetchone():
-                l.info("season %s already in database. Skipping.", season_id)
-                continue
-
-            matches_df = get_matches(season_id)
-
-            if matches_df is None:
-                l.warning("Failed to get matches for %s!", season_id)
-                continue
-            no_of_matches = matches_df["eventId"].count()
-
-            con.sql("INSERT OR IGNORE INTO Matches BY NAME SELECT * FROM matches_df")
-
-            l.info("Gettingg match data for %s matches...", no_of_matches)
-            res = []
-            for idx, match_row in matches_df.iterrows():
-                if idx % 10 == 0:
-                    l.info("Fetting match %s of %s.", idx, no_of_matches)
-                if (
-                    match_row["statusType"] == "finished"
-                    and match_row["hasOpta"] is True
-                ):  # hasOpta can be nan
-                    res.append(get_match_data(match_row["eventId"]))
-            l.info("Done getting matches.")
-
-            if not res:
-                l.warning("warning: no match data for season %s!", season_id)
-                continue
-            matchdata_df = pd.concat(res)
-            value_cols = matchdata_df.columns
-            matchdata_df.reset_index(inplace=True)
-            matchdata_df.rename(columns={"level_0": "teamId"}, inplace=True)
-            index_cols = ["teamId", "eventId"]
-            matchdata_df = pd.melt(matchdata_df, index_cols, value_cols)
-
-            con.sql("INSERT INTO MatchData SELECT * FROM matchdata_df")
-
-    l.info("Done")
-    return 0
+                );
+        """.replace("\n", "")
+    )
+    con.sql(
+        "CREATE TABLE IF NOT EXISTS MatchStats(teamId int16, eventId int64 REFERENCES Matches(eventId), variable varchar, value double);"
+    )
+    con.sql(
+        """CREATE TABLE IF NOT EXISTS MatchMomentum(
+                eventId int64 REFERENCES Matches(eventId),
+                minute int16,
+                endRecordMin int16,
+                momentumValue int16,
+                homePosessionValue double,
+                awayPosessionValue double,
+                homeMinutesWithMomentum int16,
+                awayMinutesWithMomentum int16
+                );
+                """.replace("\n", "")
+    )
+    con.sql(
+        """CREATE TABLE IF NOT EXISTS MatchXG(
+                eventId int64 REFERENCES Matches(eventId),    
+                min int16,
+                sec int8,
+                x float,
+                y float,
+                period_id int4,
+                expectedGoalsValue double,
+                situation varchar,
+                type varchar,
+                teamId int16
+                );
+                """.replace("\n", "")
+    )
--- src/slpredictions/tools/data.py
+++ src/slpredictions/tools/data.py
@@ -8,13 +8,12 @@ from pandas import DataFrame
 from .at import access_token
 
 logging.basicConfig(
-    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", 
-    level=logging.DEBUG
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", level=logging.DEBUG
 )
-l = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
-def persist_to_file(file_name="apicache.dat"):
+def _persist_to_file(file_name="apicache.dat"):
     def decorator(original_func):
         try:
             cache = Cache(file_name)
@@ -22,7 +21,7 @@ def persist_to_file(file_name="apicache.dat"):
             cache = {}
 
         def cached_func(*args, **kwargs):
-            key = str(args) + str(kwargs)
+            key = original_func.__name__ + str(args) + str(kwargs)
             if key not in cache:
                 cache[key] = original_func(*args, **kwargs)
             return cache[key]
@@ -32,21 +31,25 @@ def persist_to_file(file_name="apicache.dat"):
     return decorator
 
 
-@persist_to_file()
+@_persist_to_file()
 def get_seasons() -> DataFrame:
     try:
         r = requests.get(_get_season_endpoint())
+        if r.status_code != 200:
+            raise Exception("API dit not respond OK", r.status_code)
         json = r.json()
-        l.info("Seasons downloaded. Current season %s", json["seasonId"])
+        logger.info("Seasons downloaded. Current season %s", json["seasonId"])
         return pd.DataFrame(json["seasons"])
-    except Exception as e:
-        l.error("Error getting seasons.", exc_info=True)
+    except Exception:
+        logger.error("Error getting seasons.", exc_info=True)
 
 
-@persist_to_file()
+@_persist_to_file()
 def get_matches(season_id: int) -> DataFrame:
     try:
         r = requests.get(_get_matches_endpoint(season_id))
+        if r.status_code != 200:
+            raise Exception("API dit not respond OK", r.status_code)
         json = r.json()
         cols = [
             "tournamentId",
@@ -69,27 +72,82 @@ def get_matches(season_id: int) -> DataFrame:
         res = pd.concat([placeholder, pd.DataFrame(json["events"])])
         # filter out additional columns
         return res[cols]  # extra specification to select only wanted cols
-    except Exception as e:
-        l.error("Error while getting match details.", exc_info=True)
+    except Exception:
+        logger.error("Error while getting match details.", exc_info=True)
 
 
-@persist_to_file()
-def get_match_data(event_id: int) -> DataFrame:
+@_persist_to_file()
+def get_match_stats(event_id: int) -> DataFrame:
     try:
         r = requests.get(_get_match_data_endpoint(event_id))
+        if r.status_code != 200:
+            raise Exception("API dit not respond OK", r.status_code)
         json = r.json()
         spectators = json["spectators"]
         home_id = json["homeId"]
         away_id = json["awayId"]
-        homestat = pd.DataFrame(json["homeStats"], index=[home_id])
-        awaystat = pd.DataFrame(json["awayStats"], index=[away_id])
+        homestat = pd.DataFrame(json["homeStats"], index=[0])
+        homestat = homestat.assign(teamId=home_id)
+        awaystat = pd.DataFrame(json["awayStats"], index=[1])
+        awaystat = awaystat.assign(teamId=away_id)
         res = pd.concat([homestat, awaystat])
         res = res.assign(spectators=spectators)
         res = res.assign(eventId=event_id)
-        res.set_index(["eventId"], append=True, inplace=True)
+        index_cols = ["eventId", "teamId"]
+        value_cols = [col for col in res.columns if col not in index_cols]
+        res = pd.melt(res, index_cols, value_cols)
         return res
-    except Exception as e:
-        l.error("Error getting match data for match: %i", event_id, exc_info=True)
+    except Exception:
+        logger.error("Error getting match data for match: %i", event_id, exc_info=True)
+
+
+@_persist_to_file()
+def get_xg_time(event_id: int) -> DataFrame:
+    try:
+        r = requests.get(_get_xg_time_endpoint(event_id))
+        if r.status_code != 200:
+            raise Exception("API dit not respond OK", r.status_code)
+        json = r.json()
+        home_id = json["homeId"]
+        away_id = json["awayId"]
+        cols = [
+            "min",
+            "sec",
+            "x",
+            "y",
+            "period_id",
+            "expectedGoalsValue",
+            "situation",
+            "type",
+        ]
+        homestat = pd.DataFrame(json["expectedGoalsData"]["home"])[cols]
+        homestat = homestat.assign(teamId=home_id)
+        awaystat = pd.DataFrame(json["expectedGoalsData"]["away"])[cols]
+        awaystat = awaystat.assign(teamId=away_id)
+        res = pd.concat([homestat, awaystat]).assign(eventId=event_id)
+        return res
+    except Exception:
+        logger.error("Failed to get detailed xG for match id %s", event_id)
+
+@_persist_to_file()
+def get_momentum(event_id: int) -> DataFrame:
+    try:
+        r = requests.get(_get_momentum_endpoint(event_id))
+        if r.status_code != 200:
+            raise Exception("API dit not respond OK", r.status_code)
+        json = r.json()
+        res = pd.DataFrame(json["momentum"])
+        posession_value = pd.json_normalize(res["scores"]).add_suffix("PosessionValue")
+        minuttes_with_momentum = pd.json_normalize(
+            res["minutesWithMomentum"]
+        ).add_suffix("MinutesWithMomentum")
+        res = res[["minute", "endRecordMin", "momentumValue"]]
+        res = pd.concat([res, posession_value, minuttes_with_momentum], axis=1).assign(
+            eventId=event_id
+        )
+        return res
+    except Exception:
+        logger.error("Failed to get momentum for match id %s", event_id)
 
 
 # base end points
@@ -103,3 +161,11 @@ def _get_matches_endpoint(season_id: int) -> str:
 
 def _get_match_data_endpoint(match_id: int) -> str:
     return f"https://api.superliga.dk/opta-stats/events/{match_id}/teams?appName=superligadk&access_token={access_token}&env=production&locale=da"
+
+
+def _get_xg_time_endpoint(match_id: int) -> str:
+    return f"https://api.superliga.dk/opta-stats/event/{match_id}/detail-expected-goals?appName=superligadk&access_token={access_token}&env=production&locale=da"
+
+
+def _get_momentum_endpoint(match_id: int) -> str:
+    return f"https://api.superliga.dk/opta-stats/events/4192417/momentum?appName=superligadk&access_token={access_token}&env=production&locale=da"
